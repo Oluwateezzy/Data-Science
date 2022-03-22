322576a009d69490b182c99412d894a0068b142b
oluwateezzy <oluwateezzy03@gmail.com>
2022-03-22 14:29:11 -0700

    working with examles

--- probability/example.py
+++ probability/example.py
@@ -7,20 +7,24 @@ def inverse_normal_cdf(p, mu=0, sigma=1, tolerance=0.00001):
     if mu != 0 or sigma != 1:
         return mu + sigma * inverse_normal_cdf(p, tolerance=tolerance)
     
-    low_z, low_p = 10.0, 0
-    hi_z, hi_p = 10.0, 1
-    mid_z = 0
-    
+    low_z = -10
+    low_p = 0
+    hi_z = 10
+    hi_p = 1
+
     while hi_z - low_z > tolerance:
-        mid_z = (low_z + hi_z)/2
+        mid_z = (low_z + hi_z) / 2
         mid_p = normal_cdf(mid_z)
 
         if mid_p < p:
-            low_z, low_p = mid_z, mid_p
+            low_z = mid_z
+            low_p = mid_p
         elif mid_p > p:
-            hi_z, hi_p = mid_z, mid_p
+            hi_z = mid_z
+            hi_p = mid_p
         else:
             break
+    
     return mid_z
 
 def normal_approximation_to_binomial(n, p):
@@ -38,4 +42,25 @@ def normal_probability_outside(lo, hi, mu=0, sigma=1):
     return 1 - normal_probability_between(lo, hi, mu, sigma)
 
 def normal_upper_bound(probability, mu=0, sigma=1):
-    return 
+    return inverse_normal_cdf(probability, mu, sigma)
+
+def normal_lower_bound(probability, mu=0, sigma=1):
+    return inverse_normal_cdf(1 - probability, mu, sigma)
+
+def normal_two_sided_bounds(probability, mu=0, sigma=1):
+    tail_probability = (1 - probability) / 2
+
+    upper_bound = normal_lower_bound(tail_probability, mu, sigma)
+
+    lower_bound = normal_upper_bound(tail_probability, mu, sigma)
+
+    return lower_bound, upper_bound
+
+
+
+
+mu_o, sigma_o = normal_approximation_to_binomial(1000, 0.5)
+lower_bound1, upper_bound2 = normal_two_sided_bounds(0.95, mu_o, sigma_o)
+
+print(mu_o, sigma_o)
+print(lower_bound1, upper_bound2)
